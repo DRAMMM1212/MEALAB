704fb47c9224f2bd0bedd3a53e1e9f40da17b5b8
DRAMMM1212 <marco.drammis@ayes.it>
2025-06-30 02:18:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // alert("Benvenuto da MeaLab - L'arte in Bottega!");
+// Modal zoom immagini gallery
+(function() {
+// Crea la modal solo se non esiste
+let modal = document.createElement('div');
+modal.className = 'img-modal';
+modal.innerHTML = `
+<button class="img-modal-close" title="Chiudi">&times;</button>
+<img src="" alt="Opera Ingrandita">
+`;
+document.body.appendChild(modal);
+
+// Apri modal al click sulla gallery-img
+document.addEventListener('click', function(e) {
+let t = e.target;
+if (t.classList.contains('zoomable')) {
+modal.classList.add('open');
+modal.querySelector('img').src = t.src;
+}
+if (t.classList.contains('img-modal-close') || t === modal) {
+modal.classList.remove('open');
+modal.querySelector('img').src = "";
+}
+});
+
+// ESC per chiudere
+document.addEventListener('keydown', function(e) {
+if (e.key === "Escape") {
+modal.classList.remove('open');
+modal.querySelector('img').src = "";
+}
+});
+})();
